150ef1c9646260ba224bef0033885820da31b28f
Emanuel Sanchez <ejsanch07@gmail.com>
2025-03-01 14:51:06 -0600

    Finished Nav.

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -13,6 +13,7 @@
     </script>
 
 <template>
+    <RouterLink :to="{name: 'CardDetail', params: {id: username}}">
         <div class="border border-gray-500 rounded-lg shadow bg-gray-400 h-full">
             <img class="object-fill h-480 w-960" v-bind:src="faker.image.urlLoremFlickr({category: 'watch', height: 480, width: 960})" style="object-fit: cover;" />
             <div class="p-4">
@@ -20,7 +21,8 @@
                 <p class="mb-3 font-normal text-gray-800">{{ state }}, {{ city }}</p>
                 <p class="mb-3 font-normal text-gray-800">{{ bio }}</p>
                 <p class="mb-3 font-normal text-gray-800">{{ email }}</p>
-                <p class="mb-3 font-normal text-gray-800">{{ phone }}</p>
+                <p class="mb-3 font-normal text-gray-800">{{ phone }}</p>                
             </div>
        </div>
+    </RouterLink>
 </template>
